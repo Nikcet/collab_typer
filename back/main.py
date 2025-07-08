802ff449e1488bec8d6e95c7e5afe504f2a42216
Nikcet <mstislav.kr@yandex.ru>
2025-07-08 18:45:22 +0300

    init

--- back/main.py
+++ back/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from back!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from app.endpoints import router
+from app import logger
 
 
-if __name__ == "__main__":
-    main()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.success("Starting up...")
+    yield
+    logger.warning("Shutting down...")
+
+
+app = FastAPI(lifespan=lifespan, root_path="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
