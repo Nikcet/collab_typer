37badf1e18769ac603d1387997c42d35794224ef
Nikcet <mstislav.kr@yandex.ru>
2025-07-11 00:56:54 +0300

    import components

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+import Header from './components/Header'
+import InputField from './components/Input';
+import { Container, Box, Typography } from '@mui/material'
+
+
+
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Container
+      maxWidth="lg"
+      sx={{ padding: '1rem', display: 'flex', flexDirection: 'column', height: '100vh'}}>
+      <Header />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          marginTop: '1rem',
+          height: '95vh'
+        }}>
+        <InputField />
+      </Box>
+    </Container>
   )
 }
 
