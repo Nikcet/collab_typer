78b9150149ec1917da2261590ef2189299e02154
Nikcet <mstislav.kr@yandex.ru>
2025-07-16 15:15:07 +0300

    remove db dependence

--- back/app/database.py
+++ back/app/database.py
@@ -1,14 +1,13 @@
-from sqlmodel import SQLModel, create_engine, select
-from fastapi import Depends, HTTPException
+from sqlmodel import SQLModel, create_engine, select, Session as DBSession
+from fastapi import HTTPException
 from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR, HTTP_404_NOT_FOUND
 
 from app.config import get_settings
 from app.models import Session
-from app.dependencies import get_db
 from app import logger
 
 settings = get_settings()
-
+engine = None
 try:
     engine = create_engine(settings.db_url)
     logger.info(f"Database engine created: {engine}")
@@ -26,32 +25,49 @@ except Exception as e:
     raise e
 
 
-def get_session(db=Depends(get_db)) -> Session:
+def get_session() -> Session:
     session = Session()
     try:
-        db.add(session)
-        db.commit()
-        db.refresh(session)
+        with DBSession(engine) as db:
+            try:
+                db.add(session)
+                db.commit()
+                db.refresh(session)
+            except Exception as e:
+                logger.error(f"Error while create session: {e}")
+                raise HTTPException(
+                    status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Error while create session: {e}",
+                )
+            return session
     except Exception as e:
         logger.error(f"Error while create session: {e}")
         raise HTTPException(
             status_code=HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Error while create session: {e}",
         )
-    return session
 
 
-def remove_session(session_id: str, db=Depends(get_db)):
+def remove_session(session_id: str):
     try:
-        statement = select(Session).where(Session.id == session_id)
-        session = db.exec(statement).first()
-        if not session:
-            raise HTTPException(
-                status_code=HTTP_404_NOT_FOUND, detail=f"Session {session_id} not found"
-            )
-        db.remove(session)
-        db.commit()
-        logger.info(f"Session {session_id} removed")
+        with DBSession(engine) as db:
+            try:
+                statement = select(Session).where(Session.id == session_id)
+                session = db.exec(statement).first()
+                if not session:
+                    raise HTTPException(
+                        status_code=HTTP_404_NOT_FOUND,
+                        detail=f"Session {session_id} not found",
+                    )
+                db.delete(session)
+                db.commit()
+                logger.info(f"Session {session_id} removed")
+            except Exception as e:
+                logger.error(f"Error while remove session: {e}")
+                raise HTTPException(
+                    status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail=f"Error while remove session: {e}",
+                )
     except Exception as e:
         logger.error(f"Error while remove session: {e}")
         raise HTTPException(
--- back/app/dependencies.py
+++ back/app/dependencies.py
@@ -1,13 +1,12 @@
-from sqlmodel import Session as DBSession
+from sqlmodel import Session
 import redis.asyncio as aioredis
 from app.config import get_settings
-from app.models import Session
 from app.database import engine
 
 settings = get_settings()
 
 def get_db():
-    with DBSession(engine) as session:
+    with Session(engine) as session:
         yield session
 
 async def get_redis():
